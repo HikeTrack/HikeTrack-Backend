9a49908044d2dba2522b2f8b4f7b53a5ebb6ec73
Vadym <Servicepost.v@gmail.com>
2024-10-12 13:26:14 +0200

    feat: connected to the new Redis DB

--- src/main/java/com/hiketrackbackend/hiketrackbackend/config/RedisConfig.java
+++ src/main/java/com/hiketrackbackend/hiketrackbackend/config/RedisConfig.java
@@ -3,6 +3,9 @@ package com.hiketrackbackend.hiketrackbackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -17,4 +20,18 @@ public class RedisConfig {
         template.setValueSerializer(new StringRedisSerializer());
         return template;
     }
+
+//    @Bean
+//    public LettuceConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration redisConf = new RedisStandaloneConfiguration();
+//        redisConf.setHostName("kind-bison-29263.upstash.io");
+//        redisConf.setPort(6379);
+//        redisConf.setPassword("AXJPAAIjcDEzMjllYzUwN2I3NjI0ZjliYTE1NWM5YTcwMDZjNjZiZnAxMA");
+//
+//        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+//                .useSsl() // Вмикаємо SSL
+//                .build();
+//
+//        return new LettuceConnectionFactory(redisConf, clientConfig);
+//    }
 }
