0d0ae0be951d3eec9606b1b6e706770cf4c07b86
Vadym <Servicepost.v@gmail.com>
2024-10-14 13:53:38 +0200

    interim commit

--- src/main/java/com/hiketrackbackend/hiketrackbackend/controller/UserController.java
+++ src/main/java/com/hiketrackbackend/hiketrackbackend/controller/UserController.java
@@ -9,14 +9,9 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -35,17 +30,16 @@ public class UserController {
     }
 
     // TODO закрыть сесию when logout
-
+//    HttpServletRequest request,
     // TODO послать линк на востановление пароля повторно( точно так же сделать и на регистрацию)
-//    @Operation(summary = "",
-//            description = "")
-//    @PostMapping("/")
-//    public String logout(HttpServletRequest request,
-//                         Authentication authentication) {
-//        String email = authentication.getName();
-//        authenticationService.logout(request, email);
-//        return "Logged out successfully";
-//    }
+    @Operation(summary = "",
+            description = "")
+    @PostMapping("/logout")
+    public String logout(Authentication authentication) {
+        String email = authentication.getName();
+        userService.logout(request, email);
+        return "Logged out successfully";
+    }
 
     @PreAuthorize("hasAnyRole('USER', 'GUIDE', 'ADMIN')")
     @PatchMapping("/{id}")
