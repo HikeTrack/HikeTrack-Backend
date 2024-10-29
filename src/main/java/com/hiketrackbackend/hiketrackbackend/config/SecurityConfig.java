9e09d0d3fee9ad8a74eb9293788b84fa8ae6c5a7
Vadym <Servicepost.v@gmail.com>
2024-10-29 19:03:07 +0100

    deploy test

--- src/main/java/com/hiketrackbackend/hiketrackbackend/config/SecurityConfig.java
+++ src/main/java/com/hiketrackbackend/hiketrackbackend/config/SecurityConfig.java
@@ -24,7 +24,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.Arrays;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
