e31e18073e433aee9fc48dccf27a65c80fa3c605
Vadym <Servicepost.v@gmail.com>
2024-12-09 16:59:59 +0100

    feat: add possibility to search by range of params. overload spec method. add new params to search DTO

--- src/main/java/com/hiketrackbackend/hiketrackbackend/repository/SpecificationProvider.java
+++ src/main/java/com/hiketrackbackend/hiketrackbackend/repository/SpecificationProvider.java
@@ -1,9 +1,18 @@
 package com.hiketrackbackend.hiketrackbackend.repository;
 
+import java.util.Arrays;
 import org.springframework.data.jpa.domain.Specification;
 
 public interface SpecificationProvider<T> {
     String getKey();
 
-    Specification<T> getSpecification(String[] params);
+    default Specification<T> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) -> root.get(getKey())
+                .in(Arrays.stream(params).toArray());
+    }
+
+    default Specification<T> getSpecification(String[] minParam, String[] maxParam) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.between(root.get(getKey()), minParam[0], maxParam[0]);
+    }
 }
